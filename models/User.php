ddbbfe08b4d23c7121add9ea0847f86cade2de0a
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-30 09:01:06 -0500

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,1 +1,32 @@
+<?php
+include_once '../config/database.php';
+
+class User {
+    private $conn;
+    private $table_name = "users";
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function checkCredentials($username, $password) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE username = :username AND password = :password";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', md5($password));
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($data) {
+        $query = "INSERT INTO " . $this->table_name . " (username, password, email) VALUES (:username, :password, :email)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':username', $data['username']);
+        $stmt->bindParam(':password', md5($data['password']));
+        $stmt->bindParam(':email', $data['email']);
+        return $stmt->execute();
+    }
+}
+?>
 
