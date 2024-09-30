182b6ae892778cf8b0f5fbc696dcf7463355a8bc
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-30 09:00:40 -0500

    Update A&DController.php

--- controllers/A&DController.php
+++ controllers/A&DController.php
@@ -1,1 +1,24 @@
+<?php
+include_once '../models/A&DRecord.php';
+
+class ADController {
+    private $adRecord;
+
+    public function __construct() {
+        $this->adRecord = new ADRecord();
+    }
+
+    public function addRecord($data) {
+        if ($this->adRecord->create($data)) {
+            echo "A&D record added.";
+        } else {
+            echo "Error adding A&D record.";
+        }
+    }
+
+    public function viewRecords() {
+        return $this->adRecord->getAll();
+    }
+}
+?>
 
