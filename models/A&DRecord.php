cf145d68df717d18c06d4001d550d7ea5c4f6fb8
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-30 09:01:26 -0500

    Update A&DRecord.php

--- models/A&DRecord.php
+++ models/A&DRecord.php
@@ -1,1 +1,32 @@
+<?php
+include_once '../config/database.php';
+
+class ADRecord {
+    private $conn;
+    private $table_name = "ad_records";
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function create($data) {
+        $query = "INSERT INTO " . $this->table_name . " (serial_number, manufacturer, model, date_acquired) 
+                  VALUES (:serial_number, :manufacturer, :model, :date_acquired)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':serial_number', $data['serial_number']);
+        $stmt->bindParam(':manufacturer', $data['manufacturer']);
+        $stmt->bindParam(':model', $data['model']);
+        $stmt->bindParam(':date_acquired', $data['date_acquired']);
+        return $stmt->execute();
+    }
+
+    public function getAll() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
 
