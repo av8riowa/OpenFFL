85fd949a12d17dd1f8bc70fab42541265edb2107
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-09-30 09:00:20 -0500

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,1 +1,35 @@
+<?php
+include_once '../models/User.php';
+
+class UserController {
+    private $user;
+
+    public function __construct() {
+        $this->user = new User();
+    }
+
+    public function login($username, $password) {
+        $result = $this->user->checkCredentials($username, $password);
+        if ($result) {
+            $_SESSION['user_id'] = $result['id'];
+            header("Location: ../views/dashboard.php");
+        } else {
+            echo "Invalid username or password.";
+        }
+    }
+
+    public function logout() {
+        session_destroy();
+        header("Location: ../views/login.php");
+    }
+
+    public function addUser($data) {
+        if ($this->user->create($data)) {
+            echo "User added successfully.";
+        } else {
+            echo "Error adding user.";
+        }
+    }
+}
+?>
 
